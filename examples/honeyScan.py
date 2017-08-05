624313ea9b5073023313ea0fc5e7451663cfb08b
Yattaze <arthurhuia@gmail.com>
2017-08-05 13:54:27 +1200

    Update honeyScan.py

--- examples/honeyScan.py
+++ examples/honeyScan.py
@@ -1,12 +1,12 @@
 import layer4, proto
 
 while (True):
-    ip = layer4.gen()
-    if (layer4.hostAlive(ip)):
-        if (layer4.portCheck(ip, 22)):
-            if (proto.ssh.checkKippo(ip)):
-                 print '[-] Kippo Running on\t:\t'+ip
-         else:
-            print '[-] SSH Not Running on\t:\t'+ip
-    else:
-        pass
+	ip = layer4.gen()
+	if (layer4.hostAlive(ip)):
+		if (layer4.portCheck(ip, 22)):
+			if (proto.ssh.checkKippo(ip)):
+				 print '[-] Kippo Running on\t:\t'+ip
+		else:
+			print '[-] SSH Not Running on\t:\t'+ip
+	else:
+		pass
